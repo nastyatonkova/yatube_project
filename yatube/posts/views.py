b57238107b582ddd270de63859a010543acaada7
Anastasiia Tonkova <nast-ton@mail.ru>
2022-04-05 16:07:48 +0200

    Added urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Main page')
+
+
+def group_list(request):
+    return HttpResponse('List of posts grouped by groups')
+
+
+def group_posts(request, pk):
+    return HttpResponse(f'Group number {pk}')
