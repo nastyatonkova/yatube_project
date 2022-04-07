75b9b34926ebdc2c45420cd656aece3513e1a7c9
Anastasiia Tonkova <nast-ton@mail.ru>
2022-04-07 23:16:41 +0200

    updated header, footer files and added static objects

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,5 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+# Path to static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
