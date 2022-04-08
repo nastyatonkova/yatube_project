988cb54615fac4962c82c0fbf7d1bb5c3fbfa466
Anastasiia Tonkova <nast-ton@mail.ru>
2022-04-08 13:29:23 +0200

    added superuser and created test posts

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # Fields that will be displayed by admin
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    # Search interface for posts
+    search_fields = ('text',)
+    # Date filter
+    list_filter = ('pub_date',)
+    # Empty field
+    empty_value_display = '-empty-'
+
+
+# Configuration to register Post model as class PostAdmin
+admin.site.register(Post, PostAdmin)
